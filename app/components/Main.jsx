065427cda95c2340fb1b196ac1b92f802427be60
Alvaro Silva <Alvaro Silva>
2016-10-05 15:51:09 +0200

    Initial commit, laying out some components and styles

---
+++ app/components/Main.jsx
@@ -0,0 +1,21 @@
+var React = require('react');
+var Nav = require('Nav');
+
+var Main = React.createClass({
+   
+    render: function() {
+        return (
+            <div>
+            <Nav/>
+                <div>
+                    <div>
+                        <p>Main.jsx rendered.</p>
+                        {this.props.children}            
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
+
+module.exports = Main;
